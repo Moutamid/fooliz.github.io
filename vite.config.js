273e2516b8f2472db6874b99c45eacea2b5cc0e3
Moutamid <45147872+Moutamid@users.noreply.github.com>
2025-03-24 00:54:33 +0400

    update repo name

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base:'/',
+  // base:'/',
+  base: '/fooliz/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
